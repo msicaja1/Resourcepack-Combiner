a6a80e55ee3b681520a67f630bbc24ed8436802b
msicaja <msicaja1@students.avc.edu>
2024-03-03 15:13:30 -0800

    done

--- cmd_check.cpp
+++ cmd_check.cpp
@@ -4,14 +4,37 @@ using namespace std;
 
 int main()
 {
+    int i, j;
+    string lineCheck, cmd;
+
+    // Open the JSON file
     ifstream ifs;
 	ifs.open("check.txt");
 
-    int i, cmd;
-    string lineCheck;
+    // Prepare to write onto temp file
+    ofstream ofs;
+    ofs.open("cmd_temp.txt");
 
-    for (i = 0; i < 16; ++i) {
-        getline(ifs, lineCheck);
-        cout << lineCheck << endl;
+    if (!ofs) { // Error for opening file
+        cerr << "File open error\n";
+        exit(0);
+    }
+
+    else {
+        // Loop to get the custom model data
+        for (i = 0; i < 17; ++i) {
+            getline(ifs, lineCheck); // Get the currect line for reading
+            for (j = 0; j < lineCheck.length(); ++j) { // Inner loop to read line character by character
+                if (lineCheck[j] == '}') { // Stop reading once first } is read, as cmd is already obtained
+                        break;
+                }
+
+                if (lineCheck[j] >= '0' && lineCheck[j] <= '9') { // Get the cmd and copy into temp file
+                    cmd = lineCheck[j];
+                    ofs << cmd;
+                }
+            }
+            ofs << endl;
+        }
     }
 }
