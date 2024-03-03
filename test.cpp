c3b8e554559522a92e0beb42d83b98d8873e0669
msicaja <msicaja1@students.avc.edu>
2024-03-02 23:24:34 -0800

    get int

--- test.cpp
+++ test.cpp
@@ -5,5 +5,20 @@ int main()
 {
     string word = "{ \"predicate\": {\"custom_model_data\": 5853}, \"model\": \"item/items/blunt\"}";
 
-    cout << word << endl;
+    /*
+    string word;
+
+    cout << "Enter phrase with number: ";
+    cin >> word; */
+
+    int i;
+
+    for (i = 0; i < word.length(); ++i) {
+        if (word[i] >= '0' && word[i] <= '9') {
+            cout << word[i] << " ";
+        }
+    }
+
+    cout << endl;
+
 }
