f158846fe6cf49d8c1cb883ab283f912229b0565
msicaja <msicaja1@students.avc.edu>
2024-03-03 15:22:03 -0800

    method done

--- cmd_check.cpp
+++ cmd_check.cpp
@@ -2,39 +2,50 @@
 #include <fstream>
 using namespace std;
 
-int main()
+class cmdCompiler
 {
-    int i, j;
-    string lineCheck, cmd;
+    public:
+    
+    void cmdGetter() {
+        int i, j;
+        string lineCheck, cmd;
 
-    // Open the JSON file
-    ifstream ifs;
-	ifs.open("check.txt");
+        // Open the JSON file
+        ifstream ifs;
+	    ifs.open("check.txt");
 
-    // Prepare to write onto temp file
-    ofstream ofs;
-    ofs.open("cmd_temp.txt");
+        // Prepare to write onto temp file
+        ofstream ofs;
+        ofs.open("cmd_temp.txt");
 
-    if (!ofs) { // Error for opening file
-        cerr << "File open error\n";
-        exit(0);
-    }
+        if (!ofs) { // Error for opening file
+            cerr << "File open error\n";
+            exit(0);
+        }
 
-    else {
-        // Loop to get the custom model data
-        for (i = 0; i < 17; ++i) {
-            getline(ifs, lineCheck); // Get the currect line for reading
-            for (j = 0; j < lineCheck.length(); ++j) { // Inner loop to read line character by character
-                if (lineCheck[j] == '}') { // Stop reading once first } is read, as cmd is already obtained
-                        break;
+        else {
+            // Loop to get the custom model data
+            for (i = 0; i < 17; ++i) {
+                getline(ifs, lineCheck); // Get the currect line for reading
+                for (j = 0; j < lineCheck.length(); ++j) { // Inner loop to read line character by character
+                    if (lineCheck[j] == '}') { // Stop reading once first } is read, as cmd is already obtained
+                            break;
+                    }
+
+                    if (lineCheck[j] >= '0' && lineCheck[j] <= '9') { // Get the cmd and copy into temp file
+                        cmd = lineCheck[j];
+                        ofs << cmd;
+                    }
                 }
-
-                if (lineCheck[j] >= '0' && lineCheck[j] <= '9') { // Get the cmd and copy into temp file
-                    cmd = lineCheck[j];
-                    ofs << cmd;
-                }
+                ofs << endl;
             }
-            ofs << endl;
         }
     }
+};
+
+int main()
+{
+    cmdCompiler cmd1;
+
+    cmd1.cmdGetter();
 }
